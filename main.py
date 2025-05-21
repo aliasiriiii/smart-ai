804288cc467696cba4907cdb8fad25f6f40e2a28
aliasiriiii <aliasiriii@hotmail.com>
2025-05-21 08:05:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -55,4 +55,5 @@ def index():
                            job_title=job_title)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+port = int(os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=port)
